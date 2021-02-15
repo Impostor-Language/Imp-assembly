cf54c1479b2e2849369f32ff1d99003621bd5a43
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-16 00:09:43 +0300

    Create MyHector.cs

---
+++ MyHector.cs
@@ -0,0 +1,19 @@
+<index>
+<index>
+   file.componets(== hector, symbol,)
+   private float (myHector.symbol
+       file.float
+   private color (myGame)
+   MyGameBox.isAGame("gameController")
+     private.server(.server
+     game.my("symbol, server")
+ <index>
+ <index>
+      BrawlStarsPlayer.server
+       PlayerController."
+     ("Assembly.server")
+           MyBrawlGame.host
+        BrawlPlayer.exe
+     ("player.assembly")
+           MyHector == game"
+          BrawlPlayer:' ==MyBrawl.server
