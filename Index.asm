b76158f127258ac13ec6bc5f18a79a6d84c2e141
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-16 00:03:09 +0300

    Create Index.asm

---
+++ Index.asm
@@ -0,0 +1,15 @@
+org 100h
+mov 9281; index.abode:
+mol 0172; index.operator:
+   add assembly.func
+   add number(style.operator
+ mov 2929; index.window(lang.operate
+ operate 1038
+    style.operator( <bd>(s
+mol 1938; index.game
+mov 133; operator.style
+     
+  
+
+
+    
